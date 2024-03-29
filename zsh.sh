24d7f6e904843b8a529fb73f1cdde05b79fed069
goldingp <6238669+goldingp@users.noreply.github.com>
2024-03-28 23:13:37 -0600

    Create zsh.sh

---
+++ zsh.sh
@@ -0,0 +1,5 @@
+# zsh
+setopt HIST_VERIFY        # Don't execute immediately upon history expansion.
+setopt HIST_IGNORE_SPACE  # Don't record an entry starting with a space.
+setopt INC_APPEND_HISTORY # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY      # Share history between all sessions.
